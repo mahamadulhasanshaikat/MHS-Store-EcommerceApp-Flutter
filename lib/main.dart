49ddd8398d8c6ec6d41d93c3290b7464a64b9bb9
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2025-07-03 20:47:37 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mhs_store/utils/theme/theme.dart';
+import 'package:mhs_store/app.dart';
 
 void main() {
   // todo : add widgets binding
@@ -8,20 +8,5 @@ void main() {
   // todo : Initialize firebase
   // todo : Initial auth
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: MhsAppTheme.lightTheme,
-      darkTheme: MhsAppTheme.darkTheme,
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp( App());
 }
